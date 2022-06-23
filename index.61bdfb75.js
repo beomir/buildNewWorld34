c4985a2de71473e65c498ea9941c7599ac7db3eb
beomir <71552217+beomir@users.noreply.github.com>
2022-06-23 21:32:53 +0000

    Deploying to gh-pages from @ beomir/buildNewWorld34@692c53a1e9885d0df349da74270837510a86761f 🚀

---
+++ index.61bdfb75.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var e=document.querySelector(".languagesList");document.getElementById("languages").addEventListener("mouseup",(function(){hidden=e.getAttribute("hidden"),hidden?e.removeAttribute("hidden"):e.setAttribute("hidden","hidden")}))}));
+//# sourceMappingURL=index.61bdfb75.js.map
